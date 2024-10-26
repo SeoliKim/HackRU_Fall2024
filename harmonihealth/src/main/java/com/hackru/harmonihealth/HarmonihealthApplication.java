4556687b9d82e7fe4511b6897d26f553121cd398
M <115192833+Melissax0@users.noreply.github.com>
2024-10-26 12:29:26 -0400

    added base code

---
+++ harmonihealth/src/main/java/com/hackru/harmonihealth/HarmonihealthApplication.java
@@ -0,0 +1,13 @@
+package com.hackru.harmonihealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmonihealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarmonihealthApplication.class, args);
+	}
+
+}
